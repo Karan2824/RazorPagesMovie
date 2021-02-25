9c6a8fcf5569a23346d228ea82cbde56127aca7a
Karandeep Singh <sandhukarn11@gmail.com>
2021-02-25 13:21:11 +1300

    Add project files.

---
+++ RazorPagesMovie/Models/movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovie.Models
+{
+    public class movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+        8
+        [DataType(DataType.Date)]
+
+
+
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
